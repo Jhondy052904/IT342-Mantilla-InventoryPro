fbf21d57a94755f2686e3317fe860b286a46cf11
LAB24_USER ON WS38 <L24X09W38@LabHCI.net>
2026-03-06 08:22:21 +0800

    Initial Spring Boot Setup

---
+++ inventorypro/src/test/java/edu/cit/mantilla/inventorypro/InventoryproApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.mantilla.inventorypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
